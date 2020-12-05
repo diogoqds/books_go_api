d30ce3449777a93b6fdaa41cd949dfbbc3d7c87f
Diogo Queiroz <diogo.qds97@gmail.com>
2020-12-05 18:00:35 -0300

    implement getBooks and getBook

--- main.go
+++ main.go
@@ -1,12 +1,66 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
 type Book struct {
-	ID     string `json:id`
+	ID     int    `json:id`
 	Title  string `json:title`
 	Author string `json:author`
 	Year   string `json:year`
 }
 
-func main() {
+var books []Book
 
+func main() {
+	router := mux.NewRouter()
+
+	books = append(books,
+		Book{ID: 1, Title: "Golang pointers", Author: "Mr. Golang", Year: "2010"},
+		Book{ID: 2, Title: "Goroutines", Author: "Mr. Goroutine", Year: "2011"},
+		Book{ID: 3, Title: "Golang routers", Author: "Mr. Router", Year: "2012"},
+		Book{ID: 4, Title: "Golang concurrency", Author: "Mr. Currency", Year: "2013"},
+		Book{ID: 5, Title: "Golang good parts", Author: "Mr. Good", Year: "2014"},
+	)
+
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books", addBook).Methods("POST")
+	router.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":3000", router))
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	for _, book := range books {
+		if book.ID == id {
+			json.NewEncoder(w).Encode(book)
+		}
+	}
+}
+
+func addBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("add book is called")
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("update book is called")
+}
+
+func removeBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("remove book is called")
 }
